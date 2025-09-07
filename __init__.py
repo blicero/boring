b9a875912ed3b4110cc01b68b6bd130321b4a3a9
Benjamin Walkenhorst <krylon@posteo.de>
2025-09-07 21:08:44 +0200

    Add boilerplate

---
+++ __init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-09-07 20:57:41 krylon>
+#
+# /home/krylon/code/python/boring/__init__.py
+# created on 07. 09. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of the PyKuang network scanner. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+boring.__init__
+
+(c) 2025 Benjamin Walkenhorst
+"""
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
